32df81b7212950ab1ce0aed8309782d8c39c8610
carlosiglesias1 <carlos.iglesiasgomez@WIRTZ.LOCAL>
2021-02-03 20:46:23 +0100

    commit on master

--- tema5/src/ejercicios/Ejercicio9.java
+++ tema5/src/ejercicios/Ejercicio9.java
@@ -1,6 +1,15 @@
 package ejercicios;
 
+/**
+ * 5.9. El siguiente programa tiene una variable global llamada saldo sobre la
+ * que se ejecutan las funciones ingresar() y retirar() que aumentan y reducen
+ * el saldo C.S. Desenvolvemento de Aplicación Multimedia. IES Fernando Wirtz
+ * Pág. 19 Programación (2020/2021) - Ejercicios Parte 1 - Estructuras de
+ * Control: Condiciones respectivamente, no pudiendo quedar el saldo por debajo
+ * de cero. ¿Hay algún error? ¿Cuánto vale la variable saldo al finalizar la
+ * ejecución del programa?
+ */
+
 public class Ejercicio9 {
-    
-    
+    float saldo;
 }
