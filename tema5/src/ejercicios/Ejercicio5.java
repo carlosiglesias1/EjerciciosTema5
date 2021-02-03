531c03518793047c64a4db8577ffecc93c78a12a
carlosiglesias1 <carlos.iglesiasgomez@WIRTZ.LOCAL>
2021-02-03 17:57:34 +0100

    commit on master

---
+++ tema5/src/ejercicios/Ejercicio5.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+/**
+ * 5.5. Programa que calcule el factorial de números menores de 20. El programa
+ * permite al usuario meter los números que desee y finalizará cuando meta un
+ * número menor que 1 o mayor que 20. Crea las funciones que consideres útiles y
+ * que puedas reutilizar en otros programas.
+ */
+
+public class Ejercicio5 {
+    
+}
