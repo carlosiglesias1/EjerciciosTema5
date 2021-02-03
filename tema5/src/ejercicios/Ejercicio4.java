a99b7ad3d96ba5f903ab2d8ec931535c999193aa
carlosiglesias1 <carlosig490@gmail.com>
2021-02-03 14:10:38 +0100

    commit on master

--- tema5/src/ejercicios/Ejercicio4.java
+++ tema5/src/ejercicios/Ejercicio4.java
@@ -11,13 +11,43 @@ import java.util.Scanner;
  * días = contarDias(fecha mayor)-contarDias(fecha menor)
  */
 public class Ejercicio4 {
+    
+    static int obtainYear(String fecha){
+        int year = 0;
+        year = Integer.parseInt(fecha.substring(fecha.lastIndexOf("/")+1));
+        return year;
+    }
+    
+    static int diasEntreAnos(String fecha1, String fecha2){
+        int diferencia= 0;
+        int ano1 = 0;
+        int ano2 = 0;
+        
+        //OBTENGO LOS AÑOS DE AMBAS FECHAS
+        ano1 = obtainYear(fecha1);
+        ano2 = obtainYear(fecha2);
+        
+        //Cuento los días de cada año entre los dos años
+        if(ano1<ano2){
+            for (int i = ano1; i < ano2; i++) {
+                if(Ejercicio3.esBisiesto(i))
+                    diferencia += 366;
+                else
+                    diferencia +=365;
+            }
+        }else{
+            diferencia = 0;
+        }
+        return diferencia;
+    }
+
     static int diasComprendidos(String fecha, String fecha1){
         int dias = 0;
-        
-        dias = Ejercicio3.contarDias(fecha1)-Ejercicio3.contarDias(fecha);
-        
+        dias += diasEntreAnos(fecha, fecha1);
+        dias += Ejercicio3.contarDias(fecha1)-Ejercicio3.contarDias(fecha);
         return dias;
     }
+
     public static void main(String[] args) {
         Scanner teclado = new Scanner (System.in);
         String fecha = "";
