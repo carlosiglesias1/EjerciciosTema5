f096c40cbac00d22c0d2ddb4d80738dcc0480ea1
carlosiglesias1 <carlos.iglesiasgomez@WIRTZ.LOCAL>
2021-02-03 18:25:47 +0100

    commit on master

--- tema5/src/ejercicios/Ejercicio5.java
+++ tema5/src/ejercicios/Ejercicio5.java
@@ -1,5 +1,7 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 /**
  * 5.5. Programa que calcule el factorial de números menores de 20. El programa
  * permite al usuario meter los números que desee y finalizará cuando meta un
@@ -8,29 +10,39 @@ package ejercicios;
  */
 
 public class Ejercicio5 {
-    static boolean salir (int numero){
+    static boolean salir(int numero) {
         boolean exit = false;
-        if(numero < 1 || numero > 20)
+        if (numero < 1 || numero > 20)
             exit = true;
         return exit;
     }
 
-    static int factorial (int numero){
-        int resultado = 0;
+    static int factorial(int numero) {
+        int resultado = 1;
         int i = numero;
-        while (i != 0){
-            resultado*=i;
+        while (i != 0) {
+            resultado *= i;
             i--;
         }
         return resultado;
     }
 
-    static void menu (){
+    static void menu() {
+        Scanner teclado = new Scanner(System.in);
+        int numero = 4;
         System.out.println("\n BIENVENIDO A CÁLCULO DE FACTORIALES \n");
-        
-        System.out.println("Introduce un número para calcular su factorial");
-        
-        System.out.println("");
-    }
 
+        while (!salir(numero)) {
+            System.out.println("Introduce un número para calcular su factorial");
+            numero = teclado.nextInt();
+            if (!salir(numero)) {
+                System.out.println("El factorial de "+numero+" es "+factorial(numero));
+            }
+        }
+
+        teclado.close();
+    }
+    public static void main(String[] args) {
+        menu();
+    }
 }
