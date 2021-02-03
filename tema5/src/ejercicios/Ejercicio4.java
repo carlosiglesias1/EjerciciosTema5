f1d0b36ea2d74fe0db97a989542fd330117573a2
carlosiglesias1 <carlos.iglesiasgomez@WIRTZ.LOCAL>
2021-02-03 18:13:39 +0100

    commit on master

--- tema5/src/ejercicios/Ejercicio4.java
+++ tema5/src/ejercicios/Ejercicio4.java
@@ -11,80 +11,81 @@ import java.util.Scanner;
  * días = contarDias(fecha mayor)-contarDias(fecha menor)
  */
 public class Ejercicio4 {
-    
-    static int getYear(String fecha){
+
+    static int getYear(String fecha) {
         int year = 0;
-        year = Integer.parseInt(fecha.substring(fecha.lastIndexOf("/")+1));
+        year = Integer.parseInt(fecha.substring(fecha.lastIndexOf("/") + 1));
         return year;
     }
 
-    static int getMonth(String date){
+    static int getMonth(String date) {
         int month = 0;
-        month = Integer.parseInt(date.substring(date.indexOf("/")+1, date.lastIndexOf("/")));
+        month = Integer.parseInt(date.substring(date.indexOf("/") + 1, date.lastIndexOf("/")));
         return month;
     }
 
-    static int getDay (String date){
+    static int getDay(String date) {
         int day = 0;
-        day = Integer.parseInt(date.substring(date.lastIndexOf("/")+1));
+        day = Integer.parseInt(date.substring(date.lastIndexOf("/") + 1));
         return day;
     }
 
-    static String [] orderDate (String date1, String date2){
-        //ESTA FUNCIÓN ORDENARÁ LAS FECHAS, DE TAL FORMA QUE AUNQUE EL USUARIO INTRODUZCA LAS FECHAS DEL REVÉS, EL PROGRAMA CALCULARÁ CORRECTAMENTE LOS DÍAS
-        //LA FECHA MÁS GRANDE IRÁ EN LA PRIMERA POSICIÓN
-        
-        String date [] = new String [2];
-        if(getYear(date1)>getYear(date2)||getYear(date1)==getYear(date2)&&getMonth(date1)>getMonth(date2)||getMonth(date1)==getMonth(date2)&&getDay(date1)>getDay(date2)){
-            date [0] = date1;
-            date [1] = date2;
-        }
-        else{
-            date [0] = date2;
-            date [1] = date1;
+    static String[] orderDate(String date1, String date2) {
+        // ESTA FUNCIÓN ORDENARÁ LAS FECHAS, DE TAL FORMA QUE AUNQUE EL USUARIO
+        // INTRODUZCA LAS FECHAS DEL REVÉS, EL PROGRAMA CALCULARÁ CORRECTAMENTE LOS DÍAS
+        // LA FECHA MÁS GRANDE IRÁ EN LA PRIMERA POSICIÓN
+
+        String date[] = new String[2];
+        if (getYear(date1) > getYear(date2) || getYear(date1) == getYear(date2) && getMonth(date1) > getMonth(date2)
+                || getMonth(date1) == getMonth(date2) && getDay(date1) > getDay(date2)) {
+            date[0] = date1;
+            date[1] = date2;
+        } else {
+            date[0] = date2;
+            date[1] = date1;
         }
         return date;
     }
 
-    static int diasEntreAnos(String fecha1, String fecha2){
-        int diferencia= 0;
+    static int diasEntreAnos(String fecha1, String fecha2) {
+        int diferencia = 0;
         int ano1 = 0;
         int ano2 = 0;
-        
-        //OBTENGO LOS AÑOS DE AMBAS FECHAS
-        ano1 = getYear(orderDate(fecha1,fecha2)[1]);
+
+        // OBTENGO LOS AÑOS DE AMBAS FECHAS
+        ano1 = getYear(orderDate(fecha1, fecha2)[1]);
         ano2 = getYear(orderDate(fecha1, fecha2)[0]);
-        
-        //Cuento los días de cada año entre los dos años
-        if(ano1<ano2){
+
+        // Cuento los días de cada año entre los dos años
+        if (ano1 < ano2) {
             for (int i = ano1; i < ano2; i++) {
-                if(Ejercicio3.esBisiesto(i))
+                if (Ejercicio3.esBisiesto(i))
                     diferencia += 366;
                 else
-                    diferencia +=365;
+                    diferencia += 365;
             }
-        }else{
+        } else {
             diferencia = 0;
         }
         return diferencia;
     }
 
-    static int diasComprendidos(String fecha, String fecha1){
+    static int diasComprendidos(String fecha, String fecha1) {
         int dias = 0;
         dias += diasEntreAnos(fecha, fecha1);
-        dias += Ejercicio3.contarDias(orderDate(fecha, fecha1)[0])-Ejercicio3.contarDias(orderDate(fecha, fecha1)[1]);
+        dias += Ejercicio3.contarDias(orderDate(fecha, fecha1)[0]) - Ejercicio3.contarDias(orderDate(fecha, fecha1)[1]);
         return dias;
     }
 
     public static void main(String[] args) {
-        Scanner teclado = new Scanner (System.in);
+        Scanner teclado = new Scanner(System.in);
         String fecha = "";
-        String fecha1="";
+        String fecha1 = "";
         System.out.print("Introduce una fecha: ");
         fecha = teclado.nextLine();
         System.out.print("Introduce la segunda fecha: ");
         fecha1 = teclado.nextLine();
-        System.out.printf("Entre ambas fechas hay %d días",Math.abs(diasComprendidos(fecha, fecha1)));
+        System.out.printf("Entre ambas fechas hay %d días", Math.abs(diasComprendidos(fecha, fecha1)));
         teclado.close();
     }
 }
--- tema5/src/ejercicios/Ejercicio5.java
+++ tema5/src/ejercicios/Ejercicio5.java
@@ -8,5 +8,29 @@ package ejercicios;
  */
 
 public class Ejercicio5 {
-    
+    static boolean salir (int numero){
+        boolean exit = false;
+        if(numero < 1 || numero > 20)
+            exit = true;
+        return exit;
+    }
+
+    static int factorial (int numero){
+        int resultado = 0;
+        int i = numero;
+        while (i != 0){
+            resultado*=i;
+            i--;
+        }
+        return resultado;
+    }
+
+    static void menu (){
+        System.out.println("\n BIENVENIDO A CÁLCULO DE FACTORIALES \n");
+        
+        System.out.println("Introduce un número para calcular su factorial");
+        
+        System.out.println("");
+    }
+
 }
