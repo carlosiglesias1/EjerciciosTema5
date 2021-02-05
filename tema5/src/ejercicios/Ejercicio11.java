d0b3b9b4697d8b2f232967e3b9d7178d5ac3ece3
carlosiglesias1 <carlos.iglesiasgomez@WIRTZ.LOCAL>
2021-02-05 20:24:49 +0100

    commit on master

--- tema5/src/ejercicios/Ejercicio11.java
+++ tema5/src/ejercicios/Ejercicio11.java
@@ -2,9 +2,7 @@ package ejercicios;
 
 /**
  * 5.11. ¿Qué mostraría este código por pantalla? ¿Por qué?
- */
-public class Ejercicio11 {
-    public static void main(String[] args) {
+ *  public static void main(String[] args) {
         float saldo = 500f;
         ingresar(saldo, 600f);
         if (saldo > 1000f)
@@ -16,4 +14,10 @@ public class Ejercicio11 {
     static void ingresar(float saldo, float increm) {
         saldo += increm;
     }
+ */
+public class Ejercicio11 {
+   public static void main(String[] args) {
+       System.out.println("Imprimirá que no tiene más de 1000 euros debido a que la función 'ingresar'"+ 
+                            "no retorna el valor del saldo incrementado.");
+   }
 }
